1df2f1da319b2e2b6db4e1ce0e452bf22094ab8e
Indika Sisiranatha <indikas72@gmail.com>
2018-08-24 03:59:37 +1000

    Initial Commit

--- AngularJavaBrain/ngModel-Examples/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("ngModelExamples", []);
-
-app.controller("ExamplesCtrl", ExamplesCtrl);
-
-function ExamplesCtrl($scope){
-    $scope.textboxChange = function() {
-        console.log("Text box value change");
-    }
-}
---
+++ CrudOperationCodeFirst/Models/Enrollement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudOperationCodeFirst.Models
+{
+    public class Enrollement
+    {
+        public int EnrollementID { get; set; }
+        public Nullable<decimal> Band { get; set; }
+        public int DepatmentID { get; set; }
+        public int EmployeeID { get; set; }
+
+        public virtual Department Department { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
